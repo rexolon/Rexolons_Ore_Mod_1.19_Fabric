d680d8f635057bf66e59fece0ff7d603ae3325c6
rexolon <sarurtz1@gmail.com>
2022-07-16 17:42:03 +0200

    Initial commit

---
+++ src/main/java/io/github/rexolon/rexores/RexolonsOreMod.java
@@ -0,0 +1,15 @@
+package io.github.rexolon.rexores;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RexolonsOreMod implements ModInitializer {
+	public static final String MOD_ID = "rexores";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/io/github/rexolon/rexores/RexolonsOreModClient.java
@@ -0,0 +1,10 @@
+package io.github.rexolon.rexores;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RexolonsOreModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
